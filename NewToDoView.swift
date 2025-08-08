e0031fc0f68489efcd2b04b9d4fefc91db5fcf91
Christina Huang <christinahuang574@gmail.com>
2025-08-08 13:39:31 -0700

    finished iteration 1

---
+++ NewToDoView.swift
@@ -0,0 +1,49 @@
+//
+//  NewToDoView.swift
+//  todo
+//
+//  Created by Scholar on 8/8/25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct NewToDoView: View
+{
+    @Bindable var toDoItem: ToDoItem
+    @Environment(\.modelContext) var modelContext
+    @Binding var showNewTask: Bool
+    
+    var body: some View
+    {
+        VStack
+        {
+            Text("Task title: ")
+            TextField("Enter the task description...", text: $toDoItem.title, axis: .vertical)
+            Toggle(isOn: $toDoItem.isImportant) {
+                Text("Is this important? ")
+            }
+            
+            Button
+            {
+                addToDo()
+                showNewTask = false
+            }
+            label:
+            {
+                Text("Save")
+            }
+        }
+        .padding()
+    }
+    
+    func addToDo()
+    {
+        let toDo = ToDoItem(title: toDoItem.title, isImportant: toDoItem.isImportant)
+        modelContext.insert(toDo)
+    }
+}
+
+#Preview {
+    NewToDoView(toDoItem: ToDoItem(title: "", isImportant: false), showNewTask: .constant(false))
+}
--- todo/ContentView.swift
+++ todo/ContentView.swift
@@ -6,19 +6,74 @@
 //
 
 import SwiftUI
+import SwiftData
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+struct ContentView: View
+{
+    @State private var showNewTask = false
+    @Query var toDos: [ToDoItem]
+    @Environment(\.modelContext) var modelContext
+    
+    
+    var body: some View
+    {
+        VStack
+        {
+            HStack
+            {
+                Text("To Do List")
+                    .font(.system(size: 40))
+                    .fontWeight(.black)
+                Spacer()
+                
+                Button
+                {
+                    withAnimation{
+                        showNewTask = true
+                    }
+                    
+                }
+                label:
+                {
+                    Text("+")
+                        .font(.title)
+                        .fontWeight(.bold)
+                }
+            }
+            .padding()
+            Spacer()
+            
+            List
+            {
+                ForEach (toDos)
+                { toDoItem in
+                    if toDoItem.isImportant {
+                        Text("‼️" + toDoItem.title)
+                    } else {
+                        Text(toDoItem.title)
+                    }
+                }
+                .onDelete(perform: deleteToDo)
+            }
+            .listStyle(.plain)
+            
         }
-        .padding()
+        
+        if showNewTask
+        {
+            NewToDoView(toDoItem: ToDoItem(title: "", isImportant: false), showNewTask: $showNewTask)
+        }
+    }
+    
+    func deleteToDo(at offsets: IndexSet) {
+        for offset in offsets {
+            let toDoItem = toDos[offset]
+            modelContext.delete(toDoItem)
+        }
     }
 }
 
 #Preview {
     ContentView()
+        .modelContainer(for: ToDoItem.self, inMemory: true)
 }
